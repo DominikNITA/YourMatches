2d79a85b4301f7936ec0f0c35047819c3a574af2
dominik.nita <dominik.nita@gmail.com>
2020-03-05 14:29:41 +0100

    Local include of FootballDataApi (await nuget update)

---
+++ FootballDataApi/Models/GoalScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FootballDataApi.Models
+{
+    public class GoalScore
+    {
+        public int? HomeTeam { get; set; }
+        public int? AwayTeam { get; set; }
+    }
+}
---
+++ FootballDataApi/Models/Score.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FootballDataApi.Models
+{
+    public class Score
+    {
+        public string Winner { get; set; }
+        public string Duration { get; set; }
+        public GoalScore HalfTime { get; set; }
+        public GoalScore FullTime { get; set; }
+        public GoalScore ExtraTime { get; set; }
+        public GoalScore Penalties { get; set; }
+    }
+
+}
